c7fc1ddddf9e3ed14fc2c26ab511d9057a6cdf22
KONEKO\iri_m_000 <iri_marcanito@hotmail.com>
2018-02-08 21:12:26 -0400

    esqueleto del menu principal donde se seleccionara el juego y demas archivos correspondientes a unity

---
+++ juegos de mesa/Assets/menú/menu.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement ;
+
+public class menu : MonoBehaviour {
+
+	public void president(){
+		SceneManager.LoadScene ("PresidenteEscena");
+	}
+
+	public void exit(){
+		Application.Quit ();
+	}
+}
